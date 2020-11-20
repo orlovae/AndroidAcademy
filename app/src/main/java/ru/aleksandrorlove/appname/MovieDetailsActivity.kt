2e6db8a9da51e71995772b2ac1bd833232b0ea9f
Alexander Orlov <79202875363@ya.ru>
2020-11-20 15:59:00 +0300

    Сдеалана разметка экрана MovieDetailsActivity

---
+++ app/src/main/java/ru/aleksandrorlove/appname/MovieDetailsActivity.kt
@@ -0,0 +1,13 @@
+package ru.aleksandrorlove.appname
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MovieDetailsActivity  : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+     }
+}
