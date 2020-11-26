2cf713d3bbc8b45a31ee5ab0e71a20a59f6282c2
Alexander Orlov <79202875363@ya.ru>
2020-11-26 14:16:39 +0300

    пофиксил сохранение фрагмента при повороте экрана

--- app/src/main/java/ru/aleksandrorlove/appname/FragmentMoviesList.kt
+++ app/src/main/java/ru/aleksandrorlove/appname/FragmentMoviesList.kt
@@ -9,10 +9,6 @@ import androidx.fragment.app.Fragment
 
 class FragmentMoviesList : Fragment(), View.OnClickListener {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view: View = inflater!!.inflate(R.layout.fragment_movies_list, container, false)
         val cardView: CardView = view.findViewById(R.id.card_view)
--- app/src/main/java/ru/aleksandrorlove/appname/MainActivity.kt
+++ app/src/main/java/ru/aleksandrorlove/appname/MainActivity.kt
@@ -1,22 +1,34 @@
 package ru.aleksandrorlove.appname
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
 
     private val fragmentMoviesList = FragmentMoviesList()
+    private var fragment: Fragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fragmentManager = supportFragmentManager
-        val fragmentTransition = fragmentManager.beginTransaction()
+        if (savedInstanceState != null) {
+            fragment = supportFragmentManager.getFragment(savedInstanceState, "name")
+        } else {
+            val fragmentManager = supportFragmentManager
+            val fragmentTransition = fragmentManager.beginTransaction()
 
-        fragmentTransition.apply {
-            add(R.id.fragment_container_view, fragmentMoviesList)
-            commit()
+            fragmentTransition.apply {
+                add(R.id.fragment_container_view, fragmentMoviesList)
+                commit()
+            }
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        supportFragmentManager.putFragment(outState, "name", fragment!!)
     }
 }
