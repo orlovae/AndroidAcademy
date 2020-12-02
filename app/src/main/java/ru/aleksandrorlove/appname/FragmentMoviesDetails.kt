2b036f43938555225fd80b69914e6009e15de5fa
Alexander Orlov <79202875363@ya.ru>
2020-12-02 10:56:13 +0300

    Правки дизайна, реализовал нажатие кнопки назад

--- app/src/main/java/ru/aleksandrorlove/appname/FragmentMoviesDetails.kt
+++ app/src/main/java/ru/aleksandrorlove/appname/FragmentMoviesDetails.kt
@@ -4,10 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
-class FragmentMoviesDetails : Fragment() {
+class FragmentMoviesDetails : Fragment(), View.OnClickListener {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_movies_details, container, false)
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_movies_details, container, false)
+        val buttonBack: TextView = view.findViewById(R.id.button_back)
+        buttonBack.setOnClickListener(this)
+        return view
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.button_back -> {
+                val fragmentManager = requireActivity().supportFragmentManager
+                fragmentManager.popBackStack()
+            }
+        }
+    }
 }
