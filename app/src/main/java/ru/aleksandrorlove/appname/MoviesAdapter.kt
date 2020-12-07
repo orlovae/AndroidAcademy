1be3d45ab1587bb94f2ccfe15a99f2bac929628e
Alexander Orlov <79202875363@ya.ru>
2020-12-07 11:56:53 +0300

    Реализовал программную смену цвета звёздочек

--- app/src/main/java/ru/aleksandrorlove/appname/MoviesAdapter.kt
+++ app/src/main/java/ru/aleksandrorlove/appname/MoviesAdapter.kt
@@ -18,7 +18,12 @@ class MoviesAdapter(private val mMovies: List<Movie>) :
         val like = itemView.findViewById<ImageView>(R.id.holder_movie_image_view_like)
         val tag = itemView.findViewById<TextView>(R.id.holder_movie_text_view_tag)
 
-        //        val stars
+        val star01 = itemView.findViewById<ImageView>(R.id.holder_movie_star_01)
+        val star02 = itemView.findViewById<ImageView>(R.id.holder_movie_star_02)
+        val star03 = itemView.findViewById<ImageView>(R.id.holder_movie_star_03)
+        val star04 = itemView.findViewById<ImageView>(R.id.holder_movie_star_04)
+        val star05 = itemView.findViewById<ImageView>(R.id.holder_movie_star_05)
+
         val reviews = itemView.findViewById<TextView>(R.id.holder_movie_text_view_reviews)
         val title = itemView.findViewById<TextView>(R.id.holder_movie_text_view_title)
         val longMovie = itemView.findViewById<TextView>(R.id.holder_movie_text_view_long)
@@ -55,7 +60,7 @@ class MoviesAdapter(private val mMovies: List<Movie>) :
         val tagTextView = holder.tag
         tagTextView.text = movie.tag
 
-        //        val stars
+        setColorStars(holder, movie.stars - 1)
 
         val reviewsTextView = holder.reviews
         val textReviews = context.getString(R.string.reviews, movie.reviews)
@@ -72,5 +77,24 @@ class MoviesAdapter(private val mMovies: List<Movie>) :
 
     override fun getItemCount(): Int {
         return mMovies.size
+    }
+
+    private fun setColorStars(holder: MoviesAdapter.ViewHolder, stars: Int) {
+        val starImageViewList = mutableListOf<ImageView>(
+            holder.star01,
+            holder.star02,
+            holder.star03,
+            holder.star04,
+            holder.star05
+        )
+
+        for (i in 0..stars) {
+            starImageViewList.get(i).setColorFilter(
+                ContextCompat.getColor(
+                    starImageViewList.get(i).context,
+                    R.color.pink_light
+                ), android.graphics.PorterDuff.Mode.SRC_IN
+            )
+        }
     }
 }
