67f12f212cccb27e72bb3a8a95ed7307542c9ceb
Alexander Orlov <79202875363@ya.ru>
2021-02-18 14:32:26 +0300

    Добвил сортировку получаемого из сети списка фильмов по id

--- app/src/main/java/ru/aleksandrorlove/appname/ViewModelMoviesList.kt
+++ app/src/main/java/ru/aleksandrorlove/appname/ViewModelMoviesList.kt
@@ -1,5 +1,6 @@
 package ru.aleksandrorlove.appname
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -58,7 +59,9 @@ class ViewModelMoviesList : ViewModel() {
             }
 
             if (remoteMoviesResult is Result.Success) {
-                val remoteMovies: List<Movie> = remoteMoviesResult.data as List<Movie>
+                val unSortedRemoteMovie: List<Movie> = remoteMoviesResult.data as List<Movie>
+
+                val  remoteMovies: List<Movie> = unSortedRemoteMovie.sortedBy { it.id }
 
                 initNotification(localMovies, remoteMovies)
 
